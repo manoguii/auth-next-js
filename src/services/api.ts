ca9b4199e1a17e4b96ec19daefd2fb6a19d121f6
Guilherme David <guilhermedavidrk@gmail.com>
2023-01-24 07:13:58 -0300

    :fire: feat: interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios from 'axios' // AxiosError
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
+// let isRefreshing = false
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -9,3 +10,53 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies['nextAuth.token']}`,
   },
 })
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      if (error.response.data?.code === 'token.expired') {
+        // renova o token
+
+        cookies = parseCookies()
+
+        const { 'nextAuth.refreshToken': refreshToken } = cookies
+
+        // if (!isRefreshing) {
+        // isRefreshing = true
+
+        api
+          .post('/refresh', {
+            refreshToken,
+          })
+          .then((response) => {
+            const { token } = response.data
+
+            setCookie(undefined, 'nextAuth.token', token, {
+              maxAge: 60 * 60 * 24 * 30,
+              path: '/',
+            })
+
+            setCookie(
+              undefined,
+              'nextAuth.refreshToken',
+              response.data.refreshToken,
+              {
+                maxAge: 60 * 60 * 24 * 30,
+                path: '/',
+              },
+            )
+
+            api.defaults.headers.Authorization = `Bearer ${token}`
+          })
+        // }
+
+        // return new Promise((resolve, reject) => {})
+      } else {
+        // desloga usuario
+      }
+    }
+  },
+)
